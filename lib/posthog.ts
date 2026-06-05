dbb238304a1728fea0a08fc6d2d6457c76e83c88
Carla Urrea Stabile <carla.urrea@okta.com>
2026-06-05 16:34:24 +0200

    change flush on posthog

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -9,8 +9,8 @@ export function getPostHogClient(): PostHog | null {
   if (!client) {
     client = new PostHog(apiKey, {
       host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://eu.i.posthog.com",
-      flushAt: 10,
-      flushInterval: 5000,
+      flushAt: 1,
+      flushInterval: 0,
     });
   }
 
